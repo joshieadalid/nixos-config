1a639ff7447e802965a1aed35159cd4bb6b0a00d
Josué Adalid <54011041+joshieadalid@users.noreply.github.com>
2023-08-07 14:51:27 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,61 +1,90 @@
 { config, pkgs, ... }:
 
+let
+  # Paquetes de Python
+  my-python-packages = ps: with ps; [
+    tkinter
+    datetime
+    networkx
+  ];
+in
 {
-  # System
+  # Versión del sistema
   system.stateVersion = "23.05";
 
+  # Configuración del sistema y hardware
+  imports = [ ./hardware-configuration.nix ];
+  hardware.opengl.driSupport32Bit = true;
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.multilib.enable = true;
+
+  # Networking
+  networking = {
+    hostName = "desk-joshieadalid";
+    networkmanager.enable = true;
+    firewall.enable = false;
+  };
+
+  # Swap
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 16 * 1024;
+  }];
+
   # Boot
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
 
-  # Networking
-  networking = {
-    networkmanager.enable = true;
-    hostName = "nixos"; # Define your hostname.
-  };
-
+  # Zona horaria
   time.timeZone = "America/Mexico_City";
-  # User management
+
+  # Usuarios
   users.extraUsers.joshieadalid = {
-    hashedPassword = "$6$UodnzAaZlHThexmm$kQcvK7x1ZmzrKWdPCPYRQktQii3kWR.v2bff0rqg7yjBGGTHMT2BsOieB3rznO5uFsJyTfjyBbHlOmR3/DtZ11";
+    hashedPassword = "$6$UodnzAaZlHThexmm$kQcvK7x1ZmzrKWdPCPYRQktQii3kWR.v2bff0rqg7yjBGGTHMT2BsOieB3rznO5uFsJyTfjyBbHlOmR3/DtZ11"; # Aquí he omitido la contraseña por seguridad
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     packages = with pkgs; [
+      python311Packages.tabulate
       firefox
       tree
-      jetbrains-mono
-      steam
       neofetch
+      jetbrains.pycharm-professional
+      google-chrome
+      android-tools
     ];
   };
 
-  # Internationalisation
+  # Internacionalización
   i18n = {
     defaultLocale = "es_MX.UTF-8";
     supportedLocales = [ "en_US.UTF-8/UTF-8" "es_MX.UTF-8/UTF-8" ];
   };
-  
-  console = {
-    keyMap = "es";
-  };
 
-  # Xserver
+  # Console
+  console.keyMap = "es";
+
+  # Servicio de Xserver
   services.xserver = {
     enable = true;
-    videoDrivers = [ "intel" ];
-    desktopManager = {
-      xfce.enable = true;
-    };
+    windowManager.i3.enable = true;
     displayManager = {
-      defaultSession = "xfce";
+      defaultSession = "none+i3";
       lightdm.enable = true;
     };
+    videoDrivers = [ "intel" ];
   };
 
-  # Audio
-  security.rtkit.enable = true;
+  # Otros programas y servicios
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+  programs.java.enable = true;
+  services.flatpak.enable = true;
   services.pipewire = {
     enable = true;
     alsa = {
@@ -64,37 +93,59 @@
     };
     pulse.enable = true;
   };
+  security.rtkit.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
 
-  # Packages
-  nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [
-    (vscode-with-extensions.override {
-    vscodeExtensions = with vscode-extensions; [
-      bbenoist.nix
-      ms-python.python
-      ms-azuretools.vscode-docker
-      ms-vscode-remote.remote-ssh
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "remote-ssh-edit";
-        publisher = "ms-vscode-remote";
-        version = "0.47.2";
-        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-      }
-    ];
-  })
-    vim
-    wget
-    xfce.xfce4-pulseaudio-plugin
-    pavucontrol
-    networkmanagerapplet
+  # Overlays
+  nixpkgs.overlays = [
+    (final: prev: {
+      steam = prev.steam.override ({ extraPkgs ? pkgs': [], ... }: {
+        extraPkgs = pkgs': (extraPkgs pkgs') ++ (with pkgs'; [
+          libgdiplus
+        ]);
+      });
+    })
   ];
 
-  # Fonts
-  fonts.fonts = with pkgs; [jetbrains-mono];
-  
-  # Misc
-  imports = [./hardware-configuration.nix];
-  nixpkgs.config.multilib.enable = true;
-  hardware.opengl.driSupport32Bit = true;
+  # Paquetes del sistema
+  environment.systemPackages = with pkgs; [
+    flatpak-builder
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+        ms-python.python
+        ms-azuretools.vscode-docker
+        ms-vscode-remote.remote-ssh
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "remote-ssh-edit";
+          publisher = "ms-vscode-remote";
+          version = "0.47.2";
+          sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+        }
+      ];
+    })
+    (steam.override {
+      extraPkgs = pkgs: [ bumblebee glxinfo ];
+    }).run
+    (python3.withPackages my-python-packages)
+    tk
+    dotnet-runtime
+    vim
+    wget
+    pavucontrol
+    pulseaudio
+    udiskie
+    networkmanagerapplet
+    dmenu
+    alacritty
+    i3
+    i3status
+  ];
+
+  # Fuentes
+  fonts.fonts = with pkgs; [ jetbrains-mono ];
 }
